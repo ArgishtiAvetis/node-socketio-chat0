131a3498364ef8b1c0b683a1bf821f4f8c0218ac
Argi <argishtiavetis@gmail.com>
2017-06-28 16:15:08 -0700

    lol

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var join = require('path').join;
+
+app.set('port', 8080 || process.env.PORT);
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+users = [];
+
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('setUsername', function(data){
+    console.log(data);
+
+    if(users.indexOf(data) > -1){
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+    else{
+      users.push(data);
+      socket.emit('userSet', {username: data});
+  	  socket.emit('onlineUsers', { username: users });
+    }
+  });
+
+  socket.emit('onlineUsers', { username: users });
+
+  socket.on('msg', function(data){
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+  })
+
+});
+
+
+http.listen(app.get('port'), function(){
+  console.log('listening on localhost:' + app.get('port'));
+});
